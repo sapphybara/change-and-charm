de1750434e67ac81c03a9521959e6dd6c0af8480
wswiser101 <59670158+wswiser101@users.noreply.github.com>
2020-12-18 17:57:03 -0600

    added routing for separate pages, controlled in the navbar

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Login extends React.Component {
+  
+  render() {
+    return (
+      <div>
+        <h2>Change and Charm - Login</h2>
+        The makeover boutique
+      </div>
+    );
+  }
+}
